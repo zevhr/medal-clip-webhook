4c8c5c36e18e06eb739ef5d91c441eea6c323a33
awexxx <alex@plaguecraft.xyz>
2021-03-17 10:57:46 -0400

    Combined the scripts
    All this commit was is combining the init script and webhook script into one, makes it less messy lol

--- scripts/webhook.php
+++ scripts/webhook.php
@@ -1,4 +1,8 @@
 <?php
+   
+   $webhookURL = $_POST["webhookURL"];
+   $medalUsername  =  $_POST["medalUsername"];
+   $clipLink  =  $_POST["clipLink"];
 
 //Discord Webhook Message
 $webhookurl = "$webhookURL";
@@ -101,3 +105,13 @@ curl_close( $ch );
 @hungaryck
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0;url=../testforms/sent.html" />
+</head>
+<body>
+
+</body>
+</html>
