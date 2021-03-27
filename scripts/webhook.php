6f168aaf93c31f652466f01033b1cd6505c14e80
awexxx <alex@plaguecraft.xyz>
2021-03-27 13:26:57 -0400

    Even more big updates
    hjehheeheheehhehehehehehehehh
    
    For starters, submission forms been revamped -- looks a lot better imo
    'Sent' page has also been updated with the relevant styling
    Did a little directory cleaning since there were files that weren't even being used lol
    Removed some commented out fields in the webhook scripts that weren't being used at all
    Added GitHub Pages-hosted testing (https://awexxx.github.io/medal-clip-webhook/gh-pages), as well as updated those to be externally sent since GH does not serve anything other than static content.
    
    a lot of updates, but the webhook works a lot better now!!!!!!!

--- scripts/webhook.php
+++ scripts/webhook.php
@@ -54,11 +54,6 @@ $json_data = json_encode([
                 "text" => "Medal.tv Clip Webhook",
             ],
 
-            // Image to send
-            //"image" => [
-              //  "url" => "https://storage.plaguecraft.xyz/web-assets/logos/pc-logo.png"
-            //],
-
             // Thumbnail
             "thumbnail" => [
                 "url" => "https://media.pocketgamer.biz/2019/10/100695/medal-logo-r225x.png"
@@ -68,23 +63,6 @@ $json_data = json_encode([
             "author" => [
                 "name" => "Medal.tv Clip Webhook",
                 "url" => "https://github.com/awexxx/medal-clip-webhook"
-            ],
-
-            // Additional Fields array
-            "fields" => [
-                // Field 1
-               /* [
-                    "name" => "Medal Username",
-                    "value" => "$medalUsername",
-                    "inline" => true
-                ],
-                // Field 2
-                [
-                    "name" => "Discord",
-                    "value" => "$clipLink",
-                    "inline" => true
-                ] */
-                // Etc..
             ]
         ]
     ]
@@ -103,222 +81,71 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec( $ch );
 // If you need to debug, or find out why you can't send message uncomment line below, and execute script.
 // echo $response;
-curl_close( $ch );
-@hungaryck
+// curl_close( $ch );
 
 ?>
-
+<style>
+<?php include '../assets/css/main.css' ?>
+</style>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Medal.tv Clip Webhook Test</title>
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+  <link rel="icon" type="image/png" href="../assets/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/animate/animate.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/util.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
+        <div class="limiter">
+    <div class="container-login100" style="background-image: url('../assets/images/bg-02.jpg');">
+      <div class="wrap-login100" style="text-align:center;">
+        <h2>You did it!</h2>
+        <br /><p style="color:black; font-size:20px;">You shared a clip to a server! Poggers!<br />
+        Check the server for <a style="color:white;" href="<?php echo $clipLink ?>">the clip you shared ;)</a></p>
+        <p><a style="color:white; font-size:20px;" href="#" onclick="history.back()">Head Back</a></p>
 
-<h2>Your clip test has been sent!</h2>
-  <p>Check your server where you had the webhook created!<br>
-    <a href="#" onclick="history.back()">Back</a></p>
-    <p><a href="<?php echo $clipLink ?>">View the clip you shared here!</a></p>
+        <hr>
+
+        <p style="color:black;"><i>Have problems? <a href="https://github.com/awexxx/medal-clip-webhook/issues">Open an issue!</i></p>
+        <p style="color:black;"><i><a href="https://github.com/awexxx/medal-clip-webhook">Fork this project!</a></p>
+
+      </div>
+    </div>
+  </div>
 </body>
 </html>
 
-  <style>
-/*Universal*/
-
-#logo{
-  float:center;
-}
-
-hr{
-  width:100px;
-  border-color:black;
-
-}
-
-.center {
-  display:block;
-  margin:0 auto;
-  margin-left: auto;
-  margin-right: auto;
-  width:20%;
-  padding-top:20px;
-}
-
-#center {
-  display:block;
-  margin:0 auto;
-  margin-left: auto;
-  margin-right: auto;
-  width:30%;
-}
-
-body{
-  background-color:#e0e0e0;
-  text-align:center;
-  padding-top:150px;
-}
-
-  h1{
-    text-align:center;
-    color:#6b6b6b;
+<style type="text/css">
+  a{
+    color:white;
   }
 
-  h2{
-    text-align:center;
-    color:#6b6b6b;
+  a:hover{
+    color:black;
   }
 
-  h3{
-    text-align:center;
-    color:#6b6b6b;
+  hr{
+    border-color:white;
   }
-
-  i{
-    font-size:20px;
-  }
-
-  body p{
-  text-align:center;
-  color:#6b6b6b;
-  font-size:20px;
-}
-
-a{
-  color:black;
-}
-
-a:hover{
-  color:white;
-}
-
-/*Home*/
-#welcome{
-  background-color:#474747;
-  padding-top:20px;
-  padding-bottom:15px;
-}
-
-#welcome h2{
-  color:#c2c2c2;
-}
-
-#welcome p{
-  color:#c2c2c2;
-}
-
-/*Socials Color Changing*/
-#medal a{
-  color:orange;
-  text-decoration:none;
-  font-size:28px;
-  padding-right:15px;
-  text-align:center;
-}
-
-#medal a:hover{
-  color:white;
-}
-
-#twitter a{
-  color:#467175;
-  text-decoration:none;
-  font-size:28px;
-  padding-right:15px;
-  padding-bottom:20px;
-  text-align:center;
-}
-
-#twitter a:hover{
-  color:white;
-}
-
-/*Navbar Attributes*/
-#navbar{
-  background-color:#e0e0e0;
-  color:#333;
-}
-
-#navbar ul{
-  padding:0px;
-  list-style:none;
-  text-align:center;
-}
-
-#navbar li{
-  display:inline;
-  text-align:center;
-}
-
-#navbar a{
-  color:#333;
-  text-decoration:none;
-  font-size:18px;
-  padding-right:15px;
-  text-align:center;
-}
-
-
-/*Footer Attributes*/
-.footer-p{
-  color:white;
-}
-.footer-basic {
-  padding:40px 0;
-  background-color:#bfbfbf;
-  color:#4b4c4d;
-}
-
-.footer-basic ul {
-  padding:0;
-  list-style:none;
-  text-align:center;
-  font-size:18px;
-  line-height:1.6;
-  margin-bottom:0;
-}
-
-.footer-basic li {
-  padding:0 10px;
-}
-
-.footer-basic ul a {
-  color:inherit;
-  text-decoration:none;
-  opacity:0.8;
-}
-
-.footer-basic ul a:hover {
-  opacity:1;
-}
-
-.footer-basic .social {
-  text-align:center;
-  padding-bottom:25px;
-}
-
-.footer-basic .social > a {
-  font-size:24px;
-  width:40px;
-  height:40px;
-  line-height:40px;
-  display:inline-block;
-  text-align:center;
-  border-radius:50%;
-  border:1px solid #ccc;
-  margin:0 8px;
-  color:inherit;
-  opacity:0.75;
-}
-
-.footer-basic .social > a:hover {
-  opacity:0.9;
-}
-
-.footer-basic .copyright {
-  margin-top:15px;
-  text-align:center;
-  font-size:13px;
-  color:black;
-  margin-bottom:0;
-}
 </style>
--- testforms/php-index.php
+++ testforms/php-index.php
@@ -1,231 +1,95 @@
-<?php
-include 'style.css';
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <title>Medal.tv Clip Webhook Test</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/animate/animate.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/util.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+<!--===============================================================================================-->
 <body>
+  
+  <div class="limiter">
+    <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+      <div class="wrap-login100">
+        <form class="login100-form validate-form" method="POST" action="../scripts/webhook.php">
+          <span class="login100-form-logo">
+            <i class="zmdi zmdi-fullscreen"></i>
+          </span>
 
-    <h2 style="color:white;">Test the Medal.tv Clip Webhook - developed by Awex / Notch</h2>
-  <form method="POST" action="../scripts/webhook.php"> <!-- Change this URL if it's in a different folder. ../ brings you to the folder up one level, ../../ = two etc etc -->
+          <span class="login100-form-title p-b-34 p-t-27">
+            Medal.tv Clip Webhook
+          </span>
 
-        <div class="panel">
-    <div class="state"><br><h1 style="padding-top:20px;">Medal.tv Clip Hook</h1></div>
-    <div class="form">
-      <input placeholder='Webhook URL' type="text" name="webhookURL">
-      <input placeholder='Medal Username' type="text" name="medalUsername">
-      <input placeholder='Caption' type="text" name="caption">
-      <input placeholder='Clip Link' type="text" name="clipLink">
-      <button class="login">Share!</button>
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="url" name="webhookURL" placeholder="Discord/Guilded API URL">
+            <span class="focus-input100" data-placeholder="&#xf191;"></span>
+          </div>
+
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="url" name="clipLink" placeholder="Clip Link">
+            <span class="focus-input100" data-placeholder="&#xf18e;"></span>
+          </div>
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="text" name="medalUsername" placeholder="Medal Username">
+            <span class="focus-input100" data-placeholder="&#xf207;"></span>
+          </div>
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="text" name="caption" placeholder="Caption">
+            <span class="focus-input100" data-placeholder="&#xf150;"></span>
+          </div>
+
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn">
+              <i class="zmdi zmdi-mail-send"></i>Submit
+            </button>
+          </div>
+
+          </div>
+        </form>
+      </div>
     </div>
-    <div class="fack"><a href="https://github.com/awexxx/medal-clip-webhook" target="_blank"><i class="fa fa-code-fork" aria-hidden="true"></i>Fork this project!</a><span style="color:white;"> -- </span><a href="php-plaintext.php">Plain Text Webhook</a></div>
   </div>
-</form>
+  
+
+  <div id="dropDownSelect1"></div>
+  
+<!--===============================================================================================-->
+  <script src="../assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../assets/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../assets/vendor/bootstrap/js/popper.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../assets/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../assets/vendor/daterangepicker/moment.min.js"></script>
+  <script src="../assets/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+  <script src="../assets/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+  <script src="../assets/js/main.js"></script>
+  <script src="../assets/js/slideshow.js"></script>
+
 </body>
 </html>
-
- <!--<style>
-/*Universal*/
-
-#logo{
-  float:center;
-}
-
-hr{
-  width:100px;
-  border-color:black;
-
-}
-
-.center {
-  display:block;
-  margin:0 auto;
-  margin-left: auto;
-  margin-right: auto;
-  width:20%;
-  padding-top:20px;
-}
-
-#center {
-  display:block;
-  margin:0 auto;
-  margin-left: auto;
-  margin-right: auto;
-  width:30%;
-}
-
-body{
-  background-color:#e0e0e0;
-  text-align:center;
-  padding-top:150px;
-}
-
-  h1{
-    text-align:center;
-    color:#6b6b6b;
-  }
-
-  h2{
-    text-align:center;
-    color:#6b6b6b;
-  }
-
-  h3{
-    text-align:center;
-    color:#6b6b6b;
-  }
-
-  i{
-    font-size:20px;
-  }
-
-  body p{
-  text-align:center;
-  color:#6b6b6b;
-  font-size:20px;
-}
-
-a{
-  color:black;
-}
-
-a:hover{
-  color:white;
-}
-
-/*Home*/
-#welcome{
-  background-color:#474747;
-  padding-top:20px;
-  padding-bottom:15px;
-}
-
-#welcome h2{
-  color:#c2c2c2;
-}
-
-#welcome p{
-  color:#c2c2c2;
-}
-
-/*Socials Color Changing*/
-#medal a{
-  color:orange;
-  text-decoration:none;
-  font-size:28px;
-  padding-right:15px;
-  text-align:center;
-}
-
-#medal a:hover{
-  color:white;
-}
-
-#twitter a{
-  color:#467175;
-  text-decoration:none;
-  font-size:28px;
-  padding-right:15px;
-  padding-bottom:20px;
-  text-align:center;
-}
-
-#twitter a:hover{
-  color:white;
-}
-
-/*Navbar Attributes*/
-#navbar{
-  background-color:#e0e0e0;
-  color:#333;
-}
-
-#navbar ul{
-  padding:0px;
-  list-style:none;
-  text-align:center;
-}
-
-#navbar li{
-  display:inline;
-  text-align:center;
-}
-
-#navbar a{
-  color:#333;
-  text-decoration:none;
-  font-size:18px;
-  padding-right:15px;
-  text-align:center;
-}
-
-
-/*Footer Attributes*/
-.footer-p{
-  color:white;
-}
-.footer-basic {
-  padding:40px 0;
-  background-color:#bfbfbf;
-  color:#4b4c4d;
-}
-
-.footer-basic ul {
-  padding:0;
-  list-style:none;
-  text-align:center;
-  font-size:18px;
-  line-height:1.6;
-  margin-bottom:0;
-}
-
-.footer-basic li {
-  padding:0 10px;
-}
-
-.footer-basic ul a {
-  color:inherit;
-  text-decoration:none;
-  opacity:0.8;
-}
-
-.footer-basic ul a:hover {
-  opacity:1;
-}
-
-.footer-basic .social {
-  text-align:center;
-  padding-bottom:25px;
-}
-
-.footer-basic .social > a {
-  font-size:24px;
-  width:40px;
-  height:40px;
-  line-height:40px;
-  display:inline-block;
-  text-align:center;
-  border-radius:50%;
-  border:1px solid #ccc;
-  margin:0 8px;
-  color:inherit;
-  opacity:0.75;
-}
-
-.footer-basic .social > a:hover {
-  opacity:0.9;
-}
-
-.footer-basic .copyright {
-  margin-top:15px;
-  text-align:center;
-  font-size:13px;
-  color:black;
-  margin-bottom:0;
-}
-</style>
