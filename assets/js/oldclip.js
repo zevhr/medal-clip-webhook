2875d8098a88bced662864a57f0baeffe16043e1
Alex / Awex <hi@awexxx.xyz>
2021-10-01 01:54:19 -0400

    Share old clips easier!

--- assets/js/oldclip.js
+++ assets/js/oldclip.js
@@ -7,19 +7,16 @@ async function findClip() {
         return document.getElementById(`noti`).innerHTML = `<p>No value was provided for one of the fields.</p>`
     }
 
-
     var url = `https://developers.medal.tv/v1/latest?userId=${medalid}&categoryId=${cid}&limit=1&offset=${offset}`
 
     try {
-        var data = await fetch(url, {
+        data = await fetch(url, {
             method: 'GET',
             headers: { 'Authorization': 'pub_HTIQnwR16xnrkTeaDEkNhZRbgYcfjS8m' } 
         }).then(response => response.json());
-        console.log(data.contentObjects[0])
 
-        const noti = document.getElementById(`noti`);
-
-        noti.innerHTML = `<p>Clip Title: <strong>${data.contentObjects[0].contentTitle}</strong><br><a href="${data.contentObjects[0].directClipUrl}" target="_blank">Clip Link</p>`
+        document.getElementById(`title`).innerHTML = `Clip Title: <strong>${data.contentObjects[0].contentTitle}</strong>`
+        document.getElementById(`url`).innerHTML = `<a href="${data.contentObjects[0].directClipUrl}" target="_blank">Clip Link</a>`
 
     }
     catch(error) {
@@ -28,5 +25,54 @@ async function findClip() {
         } else {
             document.getElementById(`noti`).innerHTML = `<p>Something went wrong trying to get your clip.</p>`
         }
+    }
+}
+
+function showForm() {
+    document.getElementById(`share`).style.display = `none`
+    document.getElementById(`share-form`).style.display = `block`
+}
+
+async function shareClip() {
+    var discordurl = document.getElementById(`webhook`).value;
+
+    if (!discordurl.toLowerCase().startsWith(`https://discord.com/api/webhooks`)) {
+        document.getElementById(`share-noti`).innerHTML = `<p>An incorrect URL for either Discord Webhook URL or the Clip Link was provided.</p>`
+    } else {
+        var timestamp = Math.round(data.contentObjects[0].createdTimestamp / 1000)
+
+        const embedData = {
+            "username": "Medal Clip Webhook",
+            "avatar_url": "https://cdn.medal.tv/assets/img/new-medal-logo.png",
+            "content": `A user just submitted a clip via the [Medal Clip Webhook](https://github.com/awexxx/medal-clip-webhook)!`,
+            "embeds": [
+                {
+                    "author": {
+                        "name": `${this.data.contentObjects[0].credits}`,
+                        "url": `${this.data.contentObjects[0].directClipUrl}`
+                    },
+                    "title": `${this.data.contentObjects[0].contentTitle}`,
+                    "color": 16776960,
+                    "description": `[This clip](${this.data.contentObjects[0].directClipUrl}) was created on <t:${timestamp}:d>.`,
+                    "image": {
+                        "url": `${this.data.contentObjects[0].contentThumbnail}`
+                    }
+                }
+            ]
+        }
+
+        await fetch(`${discordurl}`, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(embedData)
+        }).then(function(response) {
+            console.log(response.status)
+
+            if(response.status === 400) {
+                document.getElementById(`share-noti`).innerHTML = `<p>Something went wrong while trying to send your clip to Discord!<br>${error.message}</p>`
+            } else {
+                document.getElementById(`share-noti`).innerHTML = `<p>Success!</p>`
+            }
+        });
     }
 }
